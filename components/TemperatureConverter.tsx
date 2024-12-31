bb37a4ccd5ac78c27ab28ff74c6b6123b77caa7f
Carlos M <53409854+Cema2019@users.noreply.github.com>
2024-12-30 20:02:37 -0600

    refactor: add ts + mui

--- components/TemperatureConverter.tsx
+++ components/TemperatureConverter.tsx
@@ -1,37 +1,44 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { TextField, Box } from '@mui/material';
 
-function TemperatureConverter() {
-  const [celsius, setCelsius] = useState(0);
-  const [fahrenheit, setFahrenheit] = useState((celsius * 9/5) + 32);
+const TemperatureConverter: React.FC = () => {
+  const [celsius, setCelsius] = useState<number>(0);
+  const [fahrenheit, setFahrenheit] = useState<number>((celsius * 9/5) + 32);
 
-  const handleCelsiusChange = (e) => {
-    const newCelsius = e.target.value;
+  const handleCelsiusChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newCelsius = parseFloat(e.target.value);
     setCelsius(newCelsius);
     setFahrenheit((newCelsius * 9/5) + 32);
   };
 
-  const handleFahrenheitChange = (e) => {
-    const newFahrenheit = e.target.value;
+  const handleFahrenheitChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newFahrenheit = parseFloat(e.target.value);
     setFahrenheit(newFahrenheit);
     setCelsius((newFahrenheit - 32) * 5/9);
   };
 
   return (
-    <div>
-      <input
+    <Box display="flex" flexDirection="column" gap={2} width={300}>
+      <TextField
         id="celsius"
+        label="Celsius"
         type="number"
-        value={celsius} 
+        value={celsius}
         onChange={handleCelsiusChange}
+        variant="outlined"
+        fullWidth
       />
-      <input
+      <TextField
         id="fahrenheit"
+        label="Fahrenheit"
         type="number"
         value={fahrenheit}
         onChange={handleFahrenheitChange}
+        variant="outlined"
+        fullWidth
       />
-    </div>
+    </Box>
   );
-}
+};
 
 export default TemperatureConverter;

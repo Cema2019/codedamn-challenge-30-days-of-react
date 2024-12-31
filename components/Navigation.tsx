19fb28cdfcafb291f943f4858b70390bf65606b6
Carlos M <53409854+Cema2019@users.noreply.github.com>
2024-12-30 19:39:44 -0600

    feat: create Navigation file

---
+++ components/Navigation.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Box, Button } from "@mui/material";
+
+const Navigation: React.FC = () => {
+  return (
+    <Box sx={{ display: "flex", justifyContent: "center", gap: 2, marginY: 3 }}>
+      <Button variant="contained" component={Link} to="/">
+        Home
+      </Button>
+      <Button variant="contained" component={Link} to="/temperature">
+        Temperature Converter
+      </Button>
+      <Button variant="contained" component={Link} to="/name-picker">
+        Name Picker
+      </Button>
+      <Button variant="contained" component={Link} to="/emi">
+        EMI Calculator
+      </Button>
+    </Box>
+  );
+};
+
+export default Navigation;

81570613ece40c327ba77a9252ecdb1cd9ae46ff
Carlos M <53409854+Cema2019@users.noreply.github.com>
2025-02-13 15:05:29 -0600

    Adjust minHeight

--- components/ToggleSwitch.tsx
+++ components/ToggleSwitch.tsx
@@ -1,40 +1,48 @@
-import { useState } from "react";
-import { Box, Button } from "@mui/material";
+import { useState } from 'react';
+import { Box, Button } from '@mui/material';
 
 interface ToggleState {
-  text: "on" | "off";
+  text: 'on' | 'off';
   backgroundColor: string;
   textColor: string;
 }
 
 export default function App() {
   const [state, setState] = useState<ToggleState>({
-    text: "on",
-    backgroundColor: "black",
-    textColor: "white",
+    text: 'on',
+    backgroundColor: 'black',
+    textColor: 'white',
   });
 
   const handleStateChange = () => {
     setState((prevState) => ({
-      text: prevState.text === "on" ? "off" : "on",
-      backgroundColor: prevState.text === "on" ? "lightgray" : "black",
-      textColor: prevState.text === "on" ? "black" : "white",
+      text: prevState.text === 'on' ? 'off' : 'on',
+      backgroundColor: prevState.text === 'on' ? 'lightgray' : 'black',
+      textColor: prevState.text === 'on' ? 'black' : 'white',
     }));
   };
 
   return (
-    <Box textAlign="center" p={3}>
-    <Button
-      id="toggle"
-      variant="contained"
-      onClick={handleStateChange}
-      sx={{
-        backgroundColor: state.backgroundColor,
-        color: state.textColor,
-      }}
+    <Box
+      textAlign="center"
+      p={3}
+      minHeight="90%"
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
     >
-      {state.text}
-    </Button>
+      <Button
+        id="toggle"
+        variant="contained"
+        onClick={handleStateChange}
+        sx={{
+          backgroundColor: state.backgroundColor,
+          color: state.textColor,
+        }}
+      >
+        {state.text}
+      </Button>
     </Box>
   );
 }
